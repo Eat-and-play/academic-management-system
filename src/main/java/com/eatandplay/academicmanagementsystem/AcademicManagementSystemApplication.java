0e53a2d2da59f805af6c8e3ae2b93c130bcd8123
Tuxzx <tuxzx@outlook.com>
2021-12-30 23:40:00 +0800

    chore: init
    初始化项目以及配置

---
+++ src/main/java/com/eatandplay/academicmanagementsystem/AcademicManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.eatandplay.academicmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** application. */
+@SpringBootApplication
+public class AcademicManagementSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AcademicManagementSystemApplication.class, args);
+  }
+}
---
+++ src/test/java/com/eatandplay/academicmanagementsystem/AcademicManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatandplay.academicmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
